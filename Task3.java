a9488f450339225efbcb63dfaf3cd82fef44092e
Rohitash Nagpal <nagpal.rohitash26@gmail.com>
2019-07-16 12:15:51 +0530

    Update Task3.java

--- Task3.java
+++ Task3.java
@@ -1,16 +1,23 @@
 package epam.com;
 public class Task3 {
 
-	public static void main(String[] args) {
-		float first = Float.parseFloat(args[0]);
-		float second = Float.parseFloat(args[1]);
-		float temp;
-		if(first==0)
-		{
-			temp=((second*9)/5)+32;
-		}
-		else
-			temp=((second-32)*5)/9;
-		System.out.println(temp);
-	}
+	public static void main (String[]args)
+  {
+    float first = Float.parseFloat (args[0]);
+    float second = Float.parseFloat (args[1]);
+
+    float temp;
+    if (args.length==2)
+      {
+	if (first == 0)
+	  {
+	    temp = ((second * 9) / 5) + 32;
+	  }
+	else
+	    temp = ((second - 32) * 5) / 9;
+	System.out.println (temp);
+      }
+    else
+      System.out.println ("Only two arguments are allowed.");
+  }
 }
